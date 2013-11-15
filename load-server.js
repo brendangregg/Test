51cf0c569c8ba3b9000b0aa4526a3f1a80235cb5
Brendan Gregg <brendan.d.gregg@gmail.com>
2013-11-14 19:49:17 -0800

    more testing

---
+++ load-server.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var conns = 0;
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('G\'Day\n');
+	conns++;
+}).listen(8199, '0.0.0.0');
+
+function status(interval) {
+	setTimeout(function() {
+		console.log('Connections/sec: ' + conns / interval);
+		conns = 0;
+		status(interval);
+	}, interval * 1000);
+}
+
+console.log('Listening at http://127.0.0.1:8199/');
+
+if (process.argv.length > 2) {
+	var interval = process.argv[2];
+	console.log('Summary every ' + interval + ' secs');
+	status(interval);
+}
